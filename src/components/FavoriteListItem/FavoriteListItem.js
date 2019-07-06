93649df14c52f8a896aeaa9824ee233a8f72ac8a
Ivan Baresic <ivan.baresic@computerrock.com>
2019-07-06 13:20:28 +0200

    some ui updates

--- src/components/FavoriteListItem/FavoriteListItem.js
+++ src/components/FavoriteListItem/FavoriteListItem.js
@@ -9,12 +9,20 @@ const FavoriteListItem = ({favoriteMovie, trailer}) => {
             <div className="m-app-favorite-list-item__inner">
                 <h2>{favoriteMovie.title}</h2>
                 <div>
-                    <iframe
-                        width="420"
-                        height="315"
-                        src={`https://www.youtube.com/embed/${trailer.results[0].key}`}
-                        title="Title"
-                    />
+                    {trailer.results.length > 0
+
+                        ? (
+                            <iframe
+                                width="420"
+                                height="315"
+                                src={`https://www.youtube.com/embed/${trailer.results[0].key}`}
+                                title="Title"
+                            />
+                        )
+
+                        : <p className="m-app-favorite-list-item__no-trailer">No trailer for this favorite</p>
+
+                    }
                 </div>
             </div>
         </li>
